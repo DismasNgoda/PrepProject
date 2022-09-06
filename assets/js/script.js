d710c46c0be973488a6d98541701579f83d29798
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-09-06 22:37:24 +0300

    updates the mouseover event

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ const btn = document.getElementById('add');
 const city = document.getElementById('cityoutput')
 const temp = document.getElementById('temp')
 const wind = document.getElementById('wind')
-const add = document.getElementById('add')
 
 apik = "3045dd712ffe6e702e3245525ac7fa38"
 //Converting Farenheight to degrees Celcius
@@ -18,10 +17,11 @@ function convertion(val){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
         .then(data => {
-            var nameval = data['name']
-            var tempature = data['main']['temp']
-            var wndspd = data['wind']['speed']
+            let nameval = data['name']
+            let tempature = data['main']['temp']
+            let wndspd = data['wind']['speed']
 
+            // description.innerHTML = `Your Search Results: ${description}`
             city.innerHTML=`City: ${nameval}`
             temp.innerHTML = `Temperature: ${ convertion(tempature)} C`
             wind.innerHTML = `Wind Speed: ${wndspd} km/h`
@@ -30,7 +30,7 @@ function convertion(val){
         .catch(err => alert('There are no data for your given Country/City. Please try with a different search'))
     })
 
-add.addEventListener('mouseover', (event) => {
+btn.addEventListener('mouseover', (event) => {
         // highlight the mouseover target and sets color to Blue Grotto on when a mouse is moved over it.
         event.target.style.color = "#296090";
       
