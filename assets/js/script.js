d6c214d6df112c2d36661af7c19dda9fc1d3285b
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-09-06 09:28:05 +0300

    converts the entry to Celcius

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,11 @@ var description = document.getElementById('description')
 var temp = document.getElementById('temp')
 var wind = document.getElementById('wind')
 
-
-
 apik = "3045dd712ffe6e702e3245525ac7fa38"
-//Converting Faenheight to degrees Celcius
+//Converting Farenheight to degrees Celcius
 function convertion(val){
     return (val - 273).toFixed(2)
 }
-
-
 
 //fetch
     btn.addEventListener('click', function(){
